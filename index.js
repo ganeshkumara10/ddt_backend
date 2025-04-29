1926eba0b6761f495a3d6f94851632e6998ab54e
ganeshkumara10 <ganeshkumara10@gmail.com>
2025-04-29 13:51:15 +0530

    added headers

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
 // JWT Secret Key
 const jwtSecretKey = process.env.SECRET;
 // Middleware to validate JWT token
